06b44d94d273824711d59759d5502e66c5ad8663
YumNumm <onory0917@gmail.com>
2023-06-12 18:54:55 +0900

    docs: directory structure

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:confwebsite2023/pages/main_page.dart';
+import 'package:confwebsite2023/features/main/page/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
