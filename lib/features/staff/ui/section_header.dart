b72f1b6ccadf14c096b995ca2f7dfc02cad250a5
Ryo Takeuchi <develop@r09i.me>
2023-06-25 13:50:43 +0900

    fix(ui): Text Widget の描画範囲が切れないように修正 (#30)

--- lib/features/staff/ui/section_header.dart
+++ lib/features/staff/ui/section_header.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+/// ブラーのぼかし範囲
+const double _blurRadius = 20;
+
 /// A section header with a gradient.
 class SectionHeader extends StatelessWidget {
   const SectionHeader({required this.text, super.key});
@@ -16,13 +19,24 @@ class SectionHeader extends StatelessWidget {
           Color(0xFF56C2FF),
         ],
       ).createShader(bounds),
-      child: Text(
-        text,
-        style: const TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.w700,
-          fontSize: 80,
-          fontStyle: FontStyle.italic,
+      child: Container(
+        // FIXME: Text Widget の描画範囲から外れて文字やブラーが見切れてしまうため、現状は左右に余白を設けている
+        margin: const EdgeInsets.only(left: _blurRadius, right: _blurRadius),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 80,
+            fontStyle: FontStyle.italic,
+            shadows: [
+              Shadow(
+                color: Color(0x40FF57DD),
+                blurRadius: _blurRadius,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
         ),
       ),
     );
