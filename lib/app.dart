d787f0922af6345e40dc96d8e3c3f62d24409a4a
blendthink <admin@blendthink.dev>
2023-06-25 19:16:31 +0900

    feat: Set app theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:confwebsite2023/config.dart';
 import 'package:confwebsite2023/home_page.dart';
+import 'package:confwebsite2023/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -7,16 +8,14 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 class App extends ConsumerWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final config = ref.watch(configProvider);
     return MaterialApp(
       title: config.appTitle,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: lightTheme, // Specified but not used
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.dark,
       home: const MainPage(),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
