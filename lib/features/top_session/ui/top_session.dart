235113cb9365accfa4d003a8902812299c7eaf01
iseruuuuu <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>
2023-06-27 14:22:33 +0900

    add: mobileとデスクトップを切り出しました。

--- lib/features/top_session/ui/top_session.dart
+++ lib/features/top_session/ui/top_session.dart
@@ -1,10 +1,18 @@
 import 'package:confwebsite2023/features/top_session/ui/top_session_twitter.dart';
 import 'package:confwebsite2023/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class TopSession extends StatelessWidget {
   const TopSession({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class TopSessionDeskTop extends StatelessWidget {
+  const TopSessionDeskTop({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +23,111 @@ class TopSession extends StatelessWidget {
         //TODO FlutteerKaigi2023の下の余白の調整をする
         //TODO @OFFLINEの下の余白を調整する。
 
-        SvgPicture.asset(Assets.topSession.flutterTopSessionLogo),
-        const Padding(
-          padding: EdgeInsets.only(top: 24, bottom: 10),
-          child: Text(
-            'FlutterKaigi 2023',
-            style: TextStyle(
-              fontSize: 64,
-              fontWeight: FontWeight.w600,
-            ),
+        const Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: '@',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 100,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              TextSpan(
+                text: 'OFFLINE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 120,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+            ],
           ),
         ),
+        const SizedBox(height: 24),
+        const Text(
+          'November 10, 2023',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.16,
+          ),
+        ),
+        const SizedBox(height: 24),
+        const Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: 'in ',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              TextSpan(
+                text: '株式会社ナビタイムジャパン',
+                style: TextStyle(
+                  color: Color(0xFF9B9B9B),
+                  fontSize: 32,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 24),
+        const Text(
+          'Flutterをメインに扱う、日本で初開催の技術カンファレンス。\n'
+          'FlutterやDartの深い知見を持つ開発者によるセッションを多数企画します。',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Color(0xFFEADDFF),
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.50,
+          ),
+        ),
+        const SizedBox(height: 24),
+        TopSessionTwitter(
+          url: 'https://twitter.com/FlutterKaigi',
+          backgroundColor: const Color(0xFF381E72),
+          textColor: Colors.white,
+          image: Assets.topSession.twitterCheck,
+          title: '@FlutterKaigi',
+          subTitle: '最新情報を公式Twitterでチェック',
+        ),
+        const SizedBox(height: 20),
+        TopSessionTwitter(
+          url:
+              'https://twitter.com/share?url=https://example.com&text=#flutterkaigi',
+          backgroundColor: Colors.white,
+          textColor: const Color(0xFF381E72),
+          image: Assets.topSession.twitterTweet,
+          title: '#flutterkaigi',
+          subTitle: 'FlutterKaigi 2023をツイート',
+        ),
+      ],
+    );
+  }
+}
+
+class TopSessionMobile extends StatelessWidget {
+  const TopSessionMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //TODO アイコンにエフェクトをつける。
+        //TODO FlutteerKaigi2023の上の余白の調整をする
+        //TODO FlutteerKaigi2023の下の余白の調整をする
+        //TODO @OFFLINEの下の余白を調整する。
 
         const Text.rich(
           TextSpan(
