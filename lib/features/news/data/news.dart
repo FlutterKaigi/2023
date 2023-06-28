6f3aacc6d988e0ee2202e17f3d49cd576874fc1a
YumNumm <onory0917@icloud.com>
2023-06-28 19:22:08 +0900

    add: news Model

---
+++ lib/features/news/data/news.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'news.freezed.dart';
+part 'news.g.dart';
+
+@freezed
+class News with _$News {
+  const factory News({
+    required String text,
+    required String? url,
+    required DateTime startedAt,
+    required DateTime? endedAt,
+  }) = _News;
+
+  factory News.fromJson(Map<String, dynamic> json) => _$NewsFromJson(json);
+}
