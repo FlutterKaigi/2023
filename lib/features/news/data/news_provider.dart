16164bf94abb45911a719be46658d56f6738f00e
YumNumm <onory0917@icloud.com>
2023-06-28 19:22:38 +0900

    add: provider

---
+++ lib/features/news/data/news_provider.dart
@@ -0,0 +1,26 @@
+import 'package:confwebsite2023/features/news/data/news.dart';
+import 'package:confwebsite2023/features/news/data/news_data_source.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'news_provider.g.dart';
+
+@riverpod
+Future<List<News>> news(NewsRef ref) =>
+    ref.watch(newsDataSourceProvider).fetchNewsItems();
+
+/// `startedAt`が現在時刻よりも未来 かつ `endedAt`が現在時刻よりも過去のNewsのみを返す
+@riverpod
+Future<List<News>> currentNews(CurrentNewsRef ref) async {
+  final news = await ref.watch(newsProvider.future);
+  final now = DateTime.now();
+  return news
+      .where(
+        (e) =>
+            e.startedAt.toLocal().isBefore(now) &&
+            (e.endedAt?.toLocal().isAfter(now) ?? true),
+      )
+      .toList()
+    ..sort(
+      (a, b) => b.startedAt.compareTo(a.startedAt),
+    );
+}
