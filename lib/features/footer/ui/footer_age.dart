3e4f16dbefd82c74a00613caad3c25087cc7cf41
blendthink <admin@blendthink.dev>
2023-06-30 19:06:27 +0900

    refactor: OtherYearsLink をコンポーネント化

--- lib/features/footer/ui/footer_age.dart
+++ lib/features/footer/ui/footer_age.dart
@@ -6,58 +6,70 @@ class FooterAge extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final otherYearsLinkTextStyle = theme.textTheme.bodyLarge;
-    final otherYearsLinkSlashTextStyle = theme.textTheme.labelLarge;
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 20),
       child: Wrap(
         crossAxisAlignment: WrapCrossAlignment.center,
         children: [
-          Semantics(
-            label: '2021年のサイト',
-            enabled: true,
-            readOnly: true,
-            child: Link(
-              uri: Uri.parse('https://flutterkaigi.jp/2021/'),
-              target: LinkTarget.blank,
-              builder: (context, openLink) {
-                return TextButton(
-                  onPressed: openLink,
-                  child: Text(
-                    '2021',
-                    style: otherYearsLinkTextStyle,
-                  ),
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              '/',
-              style: otherYearsLinkSlashTextStyle,
-            ),
-          ),
-          Semantics(
-            label: '2022年のサイト',
-            enabled: true,
-            readOnly: true,
-            child: Link(
-              uri: Uri.parse('https://flutterkaigi.jp/2022/'),
-              target: LinkTarget.blank,
-              builder: (context, openLink) {
-                return TextButton(
-                  onPressed: openLink,
-                  child: Text(
-                    '2022',
-                    style: otherYearsLinkTextStyle,
-                  ),
-                );
-              },
-            ),
-          ),
+          _OtherYearsLink.$2021(),
+          _OtherYearsLinkSlash(),
+          _OtherYearsLink.$2022(),
         ],
+      ),
+    );
+  }
+}
+
+class _OtherYearsLink extends StatelessWidget {
+  const _OtherYearsLink.$2021()
+      : semanticsLabel = '2021年のサイト',
+        url = 'https://flutterkaigi.jp/2021/',
+        text = '2021';
+
+  const _OtherYearsLink.$2022()
+      : semanticsLabel = '2022年のサイト',
+        url = 'https://flutterkaigi.jp/2022/',
+        text = '2022';
+
+  final String semanticsLabel;
+  final String url;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    final otherYearsLinkTextStyle = Theme.of(context).textTheme.bodyLarge;
+    return Semantics(
+      label: semanticsLabel,
+      enabled: true,
+      readOnly: true,
+      child: Link(
+        uri: Uri.tryParse(url),
+        target: LinkTarget.blank,
+        builder: (context, openLink) {
+          return TextButton(
+            onPressed: openLink,
+            child: Text(
+              text,
+              style: otherYearsLinkTextStyle,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _OtherYearsLinkSlash extends StatelessWidget {
+  const _OtherYearsLinkSlash();
+
+  @override
+  Widget build(BuildContext context) {
+    final otherYearsLinkSlashTextStyle = Theme.of(context).textTheme.labelLarge;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Text(
+        '/',
+        style: otherYearsLinkSlashTextStyle,
       ),
     );
   }
