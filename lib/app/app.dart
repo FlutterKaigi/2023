78c656919f769770a7ad1edec4e4fb6414e52a05
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-07-12 09:44:16 +0900

    refactor: Move config.dart into app dir

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,5 @@
+import 'package:confwebsite2023/app/config.dart';
 import 'package:confwebsite2023/app/home_page.dart';
-import 'package:confwebsite2023/core/config.dart';
 import 'package:confwebsite2023/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
