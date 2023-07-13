fd11efeae4e7a8621eb9a6a30e9b4ee5f53cca67
iseruuuuu <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>
2023-07-13 17:29:55 +0900

    add: 共通のWantedを作成してみました。

---
+++ lib/core/components/wanted.dart
@@ -0,0 +1,207 @@
+import 'package:confwebsite2023/core/components/section_header.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WantedDesktop extends StatelessWidget {
+  const WantedDesktop({
+    required this.title,
+    required this.content,
+    required this.subContent,
+    required this.buttonTitle,
+    required this.image,
+    super.key,
+  });
+
+  final String title;
+  final String content;
+  final String subContent;
+  final String buttonTitle;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    final colorScheme = theme.colorScheme;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 156),
+      child: SizedBox(
+        width: 1128,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SectionHeader(
+              text: title,
+              style: GoogleFonts.poppins(
+                fontSize: 60,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w700,
+                height: 1.5,
+              ),
+            ),
+            const SizedBox(height: 24),
+            FittedBox(
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 700,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          content,
+                          style: textTheme.bodyLarge!.copyWith(
+                            color: colorScheme.onPrimaryContainer,
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+                        Text(
+                          subContent,
+                          style: textTheme.bodyMedium!.copyWith(
+                            color: colorScheme.secondary,
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+                        SizedBox(
+                          width: 250,
+                          height: 40,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: colorScheme.primary,
+                            ),
+                            onPressed: () {},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  buttonTitle,
+                                  style: textTheme.labelLarge?.copyWith(
+                                    color: colorScheme.onPrimary,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                Icon(
+                                  Icons.arrow_forward_ios,
+                                  size: 18,
+                                  color: colorScheme.onPrimary,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  FittedBox(
+                    child: SvgPicture.asset(
+                      image,
+                      width: 360,
+                      height: 360,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WantedMobile extends StatelessWidget {
+  const WantedMobile({
+    required this.title,
+    required this.content,
+    required this.subContent,
+    required this.buttonTitle,
+    required this.image,
+    super.key,
+  });
+
+  final String title;
+  final String content;
+  final String subContent;
+  final String buttonTitle;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    final colorScheme = theme.colorScheme;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SectionHeader(
+            text: title,
+            style: GoogleFonts.poppins(
+              fontSize: 48,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w700,
+              height: 1.5,
+            ),
+          ),
+          const SizedBox(height: 24),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                content,
+                style: textTheme.bodyLarge!.copyWith(
+                  color: colorScheme.onPrimaryContainer,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 24),
+              Text(
+                subContent,
+                style: textTheme.bodyMedium!.copyWith(
+                  color: colorScheme.secondary,
+                  fontSize: 14,
+                ),
+              ),
+              const SizedBox(height: 24),
+              SizedBox(
+                height: 40,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: colorScheme.primary,
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        buttonTitle,
+                        style: textTheme.labelLarge?.copyWith(
+                          color: colorScheme.onPrimary,
+                          fontSize: 14,
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 18,
+                        color: colorScheme.onPrimary,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 40),
+          SvgPicture.asset(
+            image,
+            width: 360,
+            height: 360,
+          ),
+        ],
+      ),
+    );
+  }
+}
