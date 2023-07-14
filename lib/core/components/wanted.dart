5eaf3774792a6b70f03f9c49efdbfd7448ef0f40
iseruuuuu <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>
2023-07-14 18:23:01 +0900

    add:ResponsiveWidgetをラップしたWantedWidgetのようなものを作ってみました。

--- lib/core/components/wanted.dart
+++ lib/core/components/wanted.dart
@@ -1,7 +1,49 @@
+import 'package:confwebsite2023/core/components/responsive_widget.dart';
 import 'package:confwebsite2023/core/components/section_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class WantedWidget extends StatelessWidget {
+  const WantedWidget({
+    required this.title,
+    required this.content,
+    required this.subContent,
+    required this.buttonTitle,
+    required this.image,
+    required this.onPressed,
+    super.key,
+  });
+
+  final String title;
+  final String content;
+  final String subContent;
+  final String buttonTitle;
+  final String image;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeWidget: WantedDesktop(
+        title: title,
+        content: content,
+        subContent: subContent,
+        buttonTitle: buttonTitle,
+        image: image,
+        onPressed: onPressed,
+      ),
+      smallWidget: WantedMobile(
+        title: title,
+        content: content,
+        subContent: subContent,
+        buttonTitle: buttonTitle,
+        image: image,
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
 
 class WantedDesktop extends StatelessWidget {
   const WantedDesktop({
