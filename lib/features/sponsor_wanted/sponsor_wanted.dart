fb2ae5658a6c270f519f2774da5d2a5a564fab51
YumNumm <onory0917@gmail.com>
2023-07-23 16:01:32 +0900

    fix: SponsorWanted内 コンテンツの改行修正

--- lib/features/sponsor_wanted/sponsor_wanted.dart
+++ lib/features/sponsor_wanted/sponsor_wanted.dart
@@ -12,7 +12,9 @@ class SponsorWanted extends StatelessWidget {
       title: 'Would you like to become a sponsor?',
       content:
           // ignore: lines_longer_than_80_chars
-          'FlutterKaigi 2023を一緒に盛り上げていただけるスポンサーさまを心からお待ちしております。この機会にあなたの企業や製品を、国内外のFlutterやDartに情熱を持つ技術者たちに紹介しませんか？ FlutterKaigi 2023は皆さまのご協力により、成功へと導かれます。ともにFlutterコミュニティを成長させ、盛り上げるお手伝いをお願い申し上げます。',
+          'FlutterKaigi 2023を一緒に盛り上げていただけるスポンサーさまを心からお待ちしております。この機会にあなたの企業や製品を、国内外のFlutterやDartに情熱を持つ技術者たちに紹介しませんか？\n'
+          // ignore: lines_longer_than_80_chars
+          'FlutterKaigi 2023は皆さまのご協力により、成功へと導かれます。ともにFlutterコミュニティを成長させ、盛り上げるお手伝いをお願い申し上げます。',
       buttonTitle: 'スポンサー募集について',
       image: Assets.illustrationTeamwork,
       onPressed: () async => launchUrlString(
