03fba42e4a481eb8d7e2ac0a51910aea13ba461a
YumNumm <onory0917@gmail.com>
2023-08-07 20:15:53 +0900

    add: images

--- lib/core/gen/assets.gen.dart
+++ lib/core/gen/assets.gen.dart
@@ -9,6 +9,13 @@
 
 import 'package:flutter/widgets.dart';
 
+class $AssetsDataGen {
+  const $AssetsDataGen();
+
+  $AssetsDataNewsGen get news => const $AssetsDataNewsGen();
+  $AssetsDataStaffv2Gen get staffv2 => const $AssetsDataStaffv2Gen();
+}
+
 class $AssetsIconsGen {
   const $AssetsIconsGen();
 
@@ -38,10 +45,264 @@ class $AssetsIconsGen {
       [discord, github, medium, note, qiita, twitter, zenn];
 }
 
+class $AssetsDataNewsGen {
+  const $AssetsDataNewsGen();
+
+  /// File path: assets/data/news/news.json
+  String get news => 'assets/data/news/news.json';
+
+  /// List of all assets
+  List<String> get values => [news];
+}
+
+class $AssetsDataStaffv2Gen {
+  const $AssetsDataStaffv2Gen();
+
+  $AssetsDataStaffv2ImagesGen get images => const $AssetsDataStaffv2ImagesGen();
+
+  /// File path: assets/data/staffv2/staffv2.json
+  String get staffv2 => 'assets/data/staffv2/staffv2.json';
+
+  /// File path: assets/data/staffv2/staffv2.json.bak
+  String get staffv2Json => 'assets/data/staffv2/staffv2.json.bak';
+
+  /// File path: assets/data/staffv2/tmp
+  String get tmp => 'assets/data/staffv2/tmp';
+
+  /// List of all assets
+  List<String> get values => [staffv2, staffv2Json, tmp];
+}
+
+class $AssetsDataStaffv2ImagesGen {
+  const $AssetsDataStaffv2ImagesGen();
+
+  /// File path: assets/data/staffv2/images/img_048265ca3297f4231f309dc6e7eeef03e6cf6999845d33dfca439c88bf0f69aa.png
+  AssetGenImage
+      get img048265ca3297f4231f309dc6e7eeef03e6cf6999845d33dfca439c88bf0f69aa =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_048265ca3297f4231f309dc6e7eeef03e6cf6999845d33dfca439c88bf0f69aa.png');
+
+  /// File path: assets/data/staffv2/images/img_1ad9a87669c351563338309a527e9a282463c53eba025633e76b388312324db5.JPG
+  AssetGenImage
+      get img1ad9a87669c351563338309a527e9a282463c53eba025633e76b388312324db5 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_1ad9a87669c351563338309a527e9a282463c53eba025633e76b388312324db5.JPG');
+
+  /// File path: assets/data/staffv2/images/img_2184aedee6a1025ae7009178618af9b511b28a846bf7951a07a71aa629eb1f81.png
+  AssetGenImage
+      get img2184aedee6a1025ae7009178618af9b511b28a846bf7951a07a71aa629eb1f81 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_2184aedee6a1025ae7009178618af9b511b28a846bf7951a07a71aa629eb1f81.png');
+
+  /// File path: assets/data/staffv2/images/img_22aa5a2422b4ebdf43ec3b0ef0da06e93543c2b8b540649a4bf1ab1afb40e4f1.jpg
+  AssetGenImage
+      get img22aa5a2422b4ebdf43ec3b0ef0da06e93543c2b8b540649a4bf1ab1afb40e4f1 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_22aa5a2422b4ebdf43ec3b0ef0da06e93543c2b8b540649a4bf1ab1afb40e4f1.jpg');
+
+  /// File path: assets/data/staffv2/images/img_280899159cca27fbca97f11c6b3289fb72d6b3e383cd0f6c5ec5e41dd8fa03d4.jpeg
+  AssetGenImage
+      get img280899159cca27fbca97f11c6b3289fb72d6b3e383cd0f6c5ec5e41dd8fa03d4 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_280899159cca27fbca97f11c6b3289fb72d6b3e383cd0f6c5ec5e41dd8fa03d4.jpeg');
+
+  /// File path: assets/data/staffv2/images/img_3624beaa99340508ee298f056bbb22c256ada5d10b6ad28011346cc395582747.png
+  AssetGenImage
+      get img3624beaa99340508ee298f056bbb22c256ada5d10b6ad28011346cc395582747 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_3624beaa99340508ee298f056bbb22c256ada5d10b6ad28011346cc395582747.png');
+
+  /// File path: assets/data/staffv2/images/img_3f65c1a8f66b4a901ef3b604819df8ad182bf0fd80f2cfe7b28ac1e6c0142a6e.jpg
+  AssetGenImage
+      get img3f65c1a8f66b4a901ef3b604819df8ad182bf0fd80f2cfe7b28ac1e6c0142a6e =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_3f65c1a8f66b4a901ef3b604819df8ad182bf0fd80f2cfe7b28ac1e6c0142a6e.jpg');
+
+  /// File path: assets/data/staffv2/images/img_490b1f9bcafc88c1db2ebd6e2f4a05050b83f5a84d0e861c8be33a1a59390b7f.png
+  AssetGenImage
+      get img490b1f9bcafc88c1db2ebd6e2f4a05050b83f5a84d0e861c8be33a1a59390b7f =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_490b1f9bcafc88c1db2ebd6e2f4a05050b83f5a84d0e861c8be33a1a59390b7f.png');
+
+  /// File path: assets/data/staffv2/images/img_518db034f7d6cfa283814342a00243cbe950dc0645422aab67e9647ec24b9a31.png
+  AssetGenImage
+      get img518db034f7d6cfa283814342a00243cbe950dc0645422aab67e9647ec24b9a31 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_518db034f7d6cfa283814342a00243cbe950dc0645422aab67e9647ec24b9a31.png');
+
+  /// File path: assets/data/staffv2/images/img_5b8abb2e2c10d85f4e89b61dc67b466de194d10a989d907d7137c8b9b963ce83.png
+  AssetGenImage
+      get img5b8abb2e2c10d85f4e89b61dc67b466de194d10a989d907d7137c8b9b963ce83 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_5b8abb2e2c10d85f4e89b61dc67b466de194d10a989d907d7137c8b9b963ce83.png');
+
+  /// File path: assets/data/staffv2/images/img_5d1a00a5929fe518e4a051837592ff6be2d9d523c020f5dfe2218d690aa9d2c9.JPG
+  AssetGenImage
+      get img5d1a00a5929fe518e4a051837592ff6be2d9d523c020f5dfe2218d690aa9d2c9 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_5d1a00a5929fe518e4a051837592ff6be2d9d523c020f5dfe2218d690aa9d2c9.JPG');
+
+  /// File path: assets/data/staffv2/images/img_6649e9083343c837e9d386761c55add8b992cf4f35ed7899da552fef6d20cb10.jpeg
+  AssetGenImage
+      get img6649e9083343c837e9d386761c55add8b992cf4f35ed7899da552fef6d20cb10 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_6649e9083343c837e9d386761c55add8b992cf4f35ed7899da552fef6d20cb10.jpeg');
+
+  /// File path: assets/data/staffv2/images/img_68e63ee55aeabdc5fb0e0ebd607048c1e093842fe92738993f3dc61de23347eb.jpg
+  AssetGenImage
+      get img68e63ee55aeabdc5fb0e0ebd607048c1e093842fe92738993f3dc61de23347eb =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_68e63ee55aeabdc5fb0e0ebd607048c1e093842fe92738993f3dc61de23347eb.jpg');
+
+  /// File path: assets/data/staffv2/images/img_6a9c29900df1aeb7501901417952a57d519c418031efa5f9d05a9413aba45881.jpeg
+  AssetGenImage
+      get img6a9c29900df1aeb7501901417952a57d519c418031efa5f9d05a9413aba45881 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_6a9c29900df1aeb7501901417952a57d519c418031efa5f9d05a9413aba45881.jpeg');
+
+  /// File path: assets/data/staffv2/images/img_7a6a1ad446120a4b83206c09393657e67cd73d45ebe6ecb530955c17b8e6a35b.png
+  AssetGenImage
+      get img7a6a1ad446120a4b83206c09393657e67cd73d45ebe6ecb530955c17b8e6a35b =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_7a6a1ad446120a4b83206c09393657e67cd73d45ebe6ecb530955c17b8e6a35b.png');
+
+  /// File path: assets/data/staffv2/images/img_7da3ea159f3ebed0cac2214bd18c90e533cfe99b54ed5e1fc63298613bae73de.png
+  AssetGenImage
+      get img7da3ea159f3ebed0cac2214bd18c90e533cfe99b54ed5e1fc63298613bae73de =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_7da3ea159f3ebed0cac2214bd18c90e533cfe99b54ed5e1fc63298613bae73de.png');
+
+  /// File path: assets/data/staffv2/images/img_9a836e269d6f635a2f52e9459de80f2176221ab23c7614f96b4e3cb0ef692700.png
+  AssetGenImage
+      get img9a836e269d6f635a2f52e9459de80f2176221ab23c7614f96b4e3cb0ef692700 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_9a836e269d6f635a2f52e9459de80f2176221ab23c7614f96b4e3cb0ef692700.png');
+
+  /// File path: assets/data/staffv2/images/img_9ede0f27b4fcdb758af3099a5e99bc4a60c94759a63eb5f378a9501e8bd196e4.jpeg
+  AssetGenImage
+      get img9ede0f27b4fcdb758af3099a5e99bc4a60c94759a63eb5f378a9501e8bd196e4 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_9ede0f27b4fcdb758af3099a5e99bc4a60c94759a63eb5f378a9501e8bd196e4.jpeg');
+
+  /// File path: assets/data/staffv2/images/img_ac4efe431ad0be5f71982f288efcb37c3e51d665339044705f24eafaa34fcffa.png
+  AssetGenImage
+      get imgAc4efe431ad0be5f71982f288efcb37c3e51d665339044705f24eafaa34fcffa =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_ac4efe431ad0be5f71982f288efcb37c3e51d665339044705f24eafaa34fcffa.png');
+
+  /// File path: assets/data/staffv2/images/img_b062eedd7a25b4d384049e35df05d7c3c60453ddc8e4e97f162b17dd8b7617ca.png
+  AssetGenImage
+      get imgB062eedd7a25b4d384049e35df05d7c3c60453ddc8e4e97f162b17dd8b7617ca =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_b062eedd7a25b4d384049e35df05d7c3c60453ddc8e4e97f162b17dd8b7617ca.png');
+
+  /// File path: assets/data/staffv2/images/img_b337512a2b537b1505c4cfce5531a44c2ba66f8e30e14f1e072a84d5bb46ea73.png
+  AssetGenImage
+      get imgB337512a2b537b1505c4cfce5531a44c2ba66f8e30e14f1e072a84d5bb46ea73 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_b337512a2b537b1505c4cfce5531a44c2ba66f8e30e14f1e072a84d5bb46ea73.png');
+
+  /// File path: assets/data/staffv2/images/img_b66fbd6a8de9a8e271a2479781040cd420d147ffcbfd7948608e0b5fc16679a9.png
+  AssetGenImage
+      get imgB66fbd6a8de9a8e271a2479781040cd420d147ffcbfd7948608e0b5fc16679a9 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_b66fbd6a8de9a8e271a2479781040cd420d147ffcbfd7948608e0b5fc16679a9.png');
+
+  /// File path: assets/data/staffv2/images/img_b8cc8daa83ebf07aa5c12a02ac97192c5e529141cebfe7a33d3b7c9d7a804bbe.jpeg
+  AssetGenImage
+      get imgB8cc8daa83ebf07aa5c12a02ac97192c5e529141cebfe7a33d3b7c9d7a804bbe =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_b8cc8daa83ebf07aa5c12a02ac97192c5e529141cebfe7a33d3b7c9d7a804bbe.jpeg');
+
+  /// File path: assets/data/staffv2/images/img_b9bd6cbf3a25c4c86341fdd32608ab0b70652cd93ff898cea82f4dfdedeb7fd4.jpg
+  AssetGenImage
+      get imgB9bd6cbf3a25c4c86341fdd32608ab0b70652cd93ff898cea82f4dfdedeb7fd4 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_b9bd6cbf3a25c4c86341fdd32608ab0b70652cd93ff898cea82f4dfdedeb7fd4.jpg');
+
+  /// File path: assets/data/staffv2/images/img_c381cc0f4c8d209aa5cb80efd6a0dac06695b0f5e1ffabd2b03b3047b57f7b69.png
+  AssetGenImage
+      get imgC381cc0f4c8d209aa5cb80efd6a0dac06695b0f5e1ffabd2b03b3047b57f7b69 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_c381cc0f4c8d209aa5cb80efd6a0dac06695b0f5e1ffabd2b03b3047b57f7b69.png');
+
+  /// File path: assets/data/staffv2/images/img_d39044d604ca91dcc687cc95b7fb5ce4c820fea6c1c7a3e0f2f249aa07337a31.jpg
+  AssetGenImage
+      get imgD39044d604ca91dcc687cc95b7fb5ce4c820fea6c1c7a3e0f2f249aa07337a31 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_d39044d604ca91dcc687cc95b7fb5ce4c820fea6c1c7a3e0f2f249aa07337a31.jpg');
+
+  /// File path: assets/data/staffv2/images/img_d9699f54b722a579a94cedcf30e498156ce67d1aeed52a38d1faa88c589868b0.png
+  AssetGenImage
+      get imgD9699f54b722a579a94cedcf30e498156ce67d1aeed52a38d1faa88c589868b0 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_d9699f54b722a579a94cedcf30e498156ce67d1aeed52a38d1faa88c589868b0.png');
+
+  /// File path: assets/data/staffv2/images/img_dbd333abceffe4beff1d760d974bb701d9a04303408d72b2f47cb53f918d8b21.jpg
+  AssetGenImage
+      get imgDbd333abceffe4beff1d760d974bb701d9a04303408d72b2f47cb53f918d8b21 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_dbd333abceffe4beff1d760d974bb701d9a04303408d72b2f47cb53f918d8b21.jpg');
+
+  /// File path: assets/data/staffv2/images/img_ec1615edc3952d31223be9bbf795db5cf3d6a3854fc36106146546606a108c8d.jpeg
+  AssetGenImage
+      get imgEc1615edc3952d31223be9bbf795db5cf3d6a3854fc36106146546606a108c8d =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_ec1615edc3952d31223be9bbf795db5cf3d6a3854fc36106146546606a108c8d.jpeg');
+
+  /// File path: assets/data/staffv2/images/img_f935bdbd6f92fabd97c42719a23e6825c663bf20ba82dd20c237c8922a25a557.png
+  AssetGenImage
+      get imgF935bdbd6f92fabd97c42719a23e6825c663bf20ba82dd20c237c8922a25a557 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_f935bdbd6f92fabd97c42719a23e6825c663bf20ba82dd20c237c8922a25a557.png');
+
+  /// File path: assets/data/staffv2/images/img_f9b59488e536f745c7b249aba874bda6d547224277c7052e1455fcc9b658b471.png
+  AssetGenImage
+      get imgF9b59488e536f745c7b249aba874bda6d547224277c7052e1455fcc9b658b471 =>
+          const AssetGenImage(
+              'assets/data/staffv2/images/img_f9b59488e536f745c7b249aba874bda6d547224277c7052e1455fcc9b658b471.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        img048265ca3297f4231f309dc6e7eeef03e6cf6999845d33dfca439c88bf0f69aa,
+        img1ad9a87669c351563338309a527e9a282463c53eba025633e76b388312324db5,
+        img2184aedee6a1025ae7009178618af9b511b28a846bf7951a07a71aa629eb1f81,
+        img22aa5a2422b4ebdf43ec3b0ef0da06e93543c2b8b540649a4bf1ab1afb40e4f1,
+        img280899159cca27fbca97f11c6b3289fb72d6b3e383cd0f6c5ec5e41dd8fa03d4,
+        img3624beaa99340508ee298f056bbb22c256ada5d10b6ad28011346cc395582747,
+        img3f65c1a8f66b4a901ef3b604819df8ad182bf0fd80f2cfe7b28ac1e6c0142a6e,
+        img490b1f9bcafc88c1db2ebd6e2f4a05050b83f5a84d0e861c8be33a1a59390b7f,
+        img518db034f7d6cfa283814342a00243cbe950dc0645422aab67e9647ec24b9a31,
+        img5b8abb2e2c10d85f4e89b61dc67b466de194d10a989d907d7137c8b9b963ce83,
+        img5d1a00a5929fe518e4a051837592ff6be2d9d523c020f5dfe2218d690aa9d2c9,
+        img6649e9083343c837e9d386761c55add8b992cf4f35ed7899da552fef6d20cb10,
+        img68e63ee55aeabdc5fb0e0ebd607048c1e093842fe92738993f3dc61de23347eb,
+        img6a9c29900df1aeb7501901417952a57d519c418031efa5f9d05a9413aba45881,
+        img7a6a1ad446120a4b83206c09393657e67cd73d45ebe6ecb530955c17b8e6a35b,
+        img7da3ea159f3ebed0cac2214bd18c90e533cfe99b54ed5e1fc63298613bae73de,
+        img9a836e269d6f635a2f52e9459de80f2176221ab23c7614f96b4e3cb0ef692700,
+        img9ede0f27b4fcdb758af3099a5e99bc4a60c94759a63eb5f378a9501e8bd196e4,
+        imgAc4efe431ad0be5f71982f288efcb37c3e51d665339044705f24eafaa34fcffa,
+        imgB062eedd7a25b4d384049e35df05d7c3c60453ddc8e4e97f162b17dd8b7617ca,
+        imgB337512a2b537b1505c4cfce5531a44c2ba66f8e30e14f1e072a84d5bb46ea73,
+        imgB66fbd6a8de9a8e271a2479781040cd420d147ffcbfd7948608e0b5fc16679a9,
+        imgB8cc8daa83ebf07aa5c12a02ac97192c5e529141cebfe7a33d3b7c9d7a804bbe,
+        imgB9bd6cbf3a25c4c86341fdd32608ab0b70652cd93ff898cea82f4dfdedeb7fd4,
+        imgC381cc0f4c8d209aa5cb80efd6a0dac06695b0f5e1ffabd2b03b3047b57f7b69,
+        imgD39044d604ca91dcc687cc95b7fb5ce4c820fea6c1c7a3e0f2f249aa07337a31,
+        imgD9699f54b722a579a94cedcf30e498156ce67d1aeed52a38d1faa88c589868b0,
+        imgDbd333abceffe4beff1d760d974bb701d9a04303408d72b2f47cb53f918d8b21,
+        imgEc1615edc3952d31223be9bbf795db5cf3d6a3854fc36106146546606a108c8d,
+        imgF935bdbd6f92fabd97c42719a23e6825c663bf20ba82dd20c237c8922a25a557,
+        imgF9b59488e536f745c7b249aba874bda6d547224277c7052e1455fcc9b658b471
+      ];
+}
+
 class Assets {
   Assets._();
 
   static const String illustrationTeamwork = 'assets/Illustration-Teamwork.svg';
+  static const $AssetsDataGen data = $AssetsDataGen();
   static const String flutterkaigiLogo = 'assets/flutterkaigi_logo.svg';
   static const String flutterkaigiLogoUnion =
       'assets/flutterkaigi_logo_union.svg';
