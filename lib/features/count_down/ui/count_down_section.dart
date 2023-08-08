c9f20f093c7b83aa32834449ba06a02c9e513229
ashdik <ashdik.saxophone@gmail.com>
2023-08-08 21:22:29 +0900

    feat: valueは0で全てのunitを表示

--- lib/features/count_down/ui/count_down_section.dart
+++ lib/features/count_down/ui/count_down_section.dart
@@ -1,4 +1,10 @@
+import 'package:confwebsite2023/core/foundation/iterable_ex.dart';
+import 'package:confwebsite2023/core/gen/assets.gen.dart';
+import 'package:confwebsite2023/core/theme.dart';
+import 'package:confwebsite2023/features/count_down/model/count_down_unit.dart';
+import 'package:confwebsite2023/features/count_down/ui/count_down_unit_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CountDownSection extends StatelessWidget {
   const CountDownSection({
@@ -7,6 +13,17 @@ class CountDownSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox.shrink();
+    return Row(
+      children: <Widget>[
+        ...CountDownUnit.values.map((unit) {
+          return CountDownUnitItem(
+            unit: unit,
+            value: 0,
+          );
+        })
+      ]
+          .insertingEach(() => SvgPicture.asset(Assets.icons.dot))
+          .insertingEach(() => Spaces.horizontal_40),
+    );
   }
 }
