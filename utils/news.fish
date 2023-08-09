2be29cfd7d4a03a63b4b61053f9e961942977a18
YumNumm <onory0917@gmail.com>
2023-08-10 00:09:51 +0900

    remove: news related files

--- utils/news.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-mkdir -p ./assets/data/news
-cd ./assets/data/news
-# Download JSON File from NEWT CMS
-curl "https://flutterkaigi.cdn.newt.so/v1/flutterkaigi-2023/news" -H "Authorization: Bearer $NEWT_CDN_API_TOKEN" | jq > ./news.json
-# Remove unused KV
-cat news.json | jq -c '{"items": [.items[] | del(._id, ._sys)]}' > tmp
-mv tmp news.json
