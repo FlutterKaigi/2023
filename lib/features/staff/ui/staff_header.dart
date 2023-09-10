8c7fdf6810a25714b02a13b68b9ac69f6278a0ea
Ryo Takeuchi <develop@r09i.me>
2023-09-10 20:06:31 +0900

    refactor: Gradient を外から渡せるように変更

--- lib/features/staff/ui/staff_header.dart
+++ lib/features/staff/ui/staff_header.dart
@@ -1,6 +1,6 @@
 import 'package:confwebsite2023/core/components/responsive_widget.dart';
 import 'package:confwebsite2023/core/components/section_header.dart';
-import 'package:confwebsite2023/core/theme/app_text_style.dart';
+import 'package:confwebsite2023/core/theme.dart';
 import 'package:flutter/material.dart';
 
 class StaffHeader extends StatelessWidget {
@@ -12,10 +12,12 @@ class StaffHeader extends StatelessWidget {
       largeWidget: SectionHeader(
         text: 'Staff',
         style: AppTextStyle.pcHeading1,
+        gradient: GradientConstant.accent.primary,
       ),
       smallWidget: SectionHeader(
         text: 'Staff',
         style: AppTextStyle.spHeading1,
+        gradient: GradientConstant.accent.primary,
       ),
     );
   }
