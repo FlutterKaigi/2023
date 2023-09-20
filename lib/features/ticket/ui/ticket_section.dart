877cb52057b810e5c8393396a44504b018b66def
YumNumm <onory0917@gmail.com>
2023-09-20 14:21:46 +0900

    fix: 文言の修正 不要な空白を消去

--- lib/features/ticket/ui/ticket_section.dart
+++ lib/features/ticket/ui/ticket_section.dart
@@ -9,8 +9,8 @@ class TicketSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return WantedWidget(
       title: 'Join FlutterKaigi 2023!',
-      content: 'FlutterKaigi 2023は 初のハイブリッド開催となります!\n'
-          'オフライン会場 または オンライン視聴からお選びください!\n'
+      content: 'FlutterKaigi 2023は初のハイブリッド開催となります!\n'
+          'オフライン会場またはオンライン視聴からお選びください!\n'
           'FlutterKaigi 2023のチケットをPassMarketにて販売・配布しています。\n'
           'オフライン会場チケット(ノベルティ・懇談会含む)は3,000円、オンライン配信視聴チケットは無料です。\n'
           'オンライン配信については、開催当日にライブ配信を行う予定です。配信方式について開催までに変更する可能性があります。',
