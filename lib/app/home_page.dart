8163bf72946b492fe7a518ea4387fcc8a2694167
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-09-22 17:28:03 +0900

    fix: スポンサーセクションの下にパディングを追加

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -210,6 +210,9 @@ class _MainPageBody extends StatelessWidget {
               padding: padding,
               child: const SponsorsSection(),
             ),
+            const SliverToBoxAdapter(
+              child: Spaces.vertical_200,
+            ),
             _Sliver(
               padding: padding,
               child: StaffHeader(
