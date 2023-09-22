294f8dedca03f23c2cfdad1191f4bc5500bb75c9
Ryo Takeuchi <develop@r09i.me>
2023-09-22 18:24:16 +0900

    feat: スポンサー詳細内容の Widget を追加

---
+++ lib/features/sponsor/ui/detail/sponsor_detail_content.dart
@@ -0,0 +1,94 @@
+import 'package:confwebsite2023/core/components/social_share.dart';
+import 'package:confwebsite2023/core/theme.dart';
+import 'package:confwebsite2023/features/sponsor/data/sponsor.dart';
+import 'package:confwebsite2023/features/sponsor/data/sponsor_plan.dart';
+import 'package:confwebsite2023/features/sponsor/ui/detail/sponsor_introduction.dart';
+import 'package:confwebsite2023/features/sponsor/ui/detail/sponsor_session.dart';
+import 'package:confwebsite2023/features/sponsor/ui/sponsor_plan_header.dart';
+import 'package:confwebsite2023/features/sponsor/ui/sponsors_header.dart';
+import 'package:flutter/material.dart';
+
+final class SponsorDetailContent extends StatelessWidget {
+  const SponsorDetailContent({
+    required this.sponsor,
+    required this.h1TextStyle,
+    required this.h2TextStyle,
+    required this.onTweetPressed,
+    required this.onCopyUrlPressed,
+    super.key,
+  });
+
+  final Sponsor sponsor;
+
+  final TextStyle h1TextStyle;
+  final TextStyle h2TextStyle;
+  final VoidCallback? onTweetPressed;
+  final VoidCallback? onCopyUrlPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SponsorsHeader(
+          style: h1TextStyle,
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: SocialShare(
+            onTweetPressed: onTweetPressed,
+            onCopyUrlPressed: onCopyUrlPressed,
+          ),
+        ),
+        Spaces.vertical_20,
+        Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            color: Color(0x22000000),
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              children: [
+                SponsorPlanHeader(
+                  text: switch (sponsor.plan) {
+                    SponsorPlan.platinum => 'Platinum Sponsor',
+                    SponsorPlan.gold => 'Gold Sponsor',
+                    SponsorPlan.silver => 'Silver Sponsor',
+                  },
+                  style: h2TextStyle,
+                  plan: sponsor.plan,
+                ),
+                Column(
+                  children: [
+                    SponsorIntroduction(
+                      assetName: sponsor.logoAssetName,
+                      name: sponsor.displayName,
+                      url: sponsor.url,
+                      introduction: sponsor.introduction,
+                    ),
+                    Spaces.vertical_40,
+                    if (sponsor.session != null) ...[
+                      const Divider(height: 1),
+                      Spaces.vertical_40,
+                      SponsorSessionSection(session: sponsor.session!),
+                    ],
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Spaces.vertical_20,
+        SizedBox(
+          width: double.infinity,
+          child: SocialShare(
+            onTweetPressed: onTweetPressed,
+            onCopyUrlPressed: onCopyUrlPressed,
+          ),
+        ),
+      ],
+    );
+  }
+}
