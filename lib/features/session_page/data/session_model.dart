2224bbf117ca957184bbb3d504357a11eff4ae98
iseruuuuu <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>
2023-09-23 12:02:41 +0900

    add: 仮で必要なModelを作成しました（後で削除予定）

---
+++ lib/features/session_page/data/session_model.dart
@@ -0,0 +1,22 @@
+import 'package:confwebsite2023/features/session/model/talk_user.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'session_model.freezed.dart';
+
+@freezed
+class SessionModel with _$SessionModel {
+  const factory SessionModel({
+    required String sessionName,
+    required String title,
+    required TalkUser user,
+    required String contents,
+    required String time,
+    required String trackName,
+    required String twitter,
+    required bool isSponsor,
+    required String tweet,
+    required String forteeUrl,
+    String? sponsorImage,
+    String? sponsorName,
+  }) = _SessionModel;
+}
---
+++ lib/features/session_page/data/session_model.freezed.dart
@@ -0,0 +1,389 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'session_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$SessionModel {
+  String get sessionName => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  TalkUser get user => throw _privateConstructorUsedError;
+  String get contents => throw _privateConstructorUsedError;
+  String get time => throw _privateConstructorUsedError;
+  String get trackName => throw _privateConstructorUsedError;
+  String get twitter => throw _privateConstructorUsedError;
+  bool get isSponsor => throw _privateConstructorUsedError;
+  String get tweet => throw _privateConstructorUsedError;
+  String get forteeUrl => throw _privateConstructorUsedError;
+  String? get sponsorImage => throw _privateConstructorUsedError;
+  String? get sponsorName => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $SessionModelCopyWith<SessionModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SessionModelCopyWith<$Res> {
+  factory $SessionModelCopyWith(
+          SessionModel value, $Res Function(SessionModel) then) =
+      _$SessionModelCopyWithImpl<$Res, SessionModel>;
+  @useResult
+  $Res call(
+      {String sessionName,
+      String title,
+      TalkUser user,
+      String contents,
+      String time,
+      String trackName,
+      String twitter,
+      bool isSponsor,
+      String tweet,
+      String forteeUrl,
+      String? sponsorImage,
+      String? sponsorName});
+
+  $TalkUserCopyWith<$Res> get user;
+}
+
+/// @nodoc
+class _$SessionModelCopyWithImpl<$Res, $Val extends SessionModel>
+    implements $SessionModelCopyWith<$Res> {
+  _$SessionModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? sessionName = null,
+    Object? title = null,
+    Object? user = null,
+    Object? contents = null,
+    Object? time = null,
+    Object? trackName = null,
+    Object? twitter = null,
+    Object? isSponsor = null,
+    Object? tweet = null,
+    Object? forteeUrl = null,
+    Object? sponsorImage = freezed,
+    Object? sponsorName = freezed,
+  }) {
+    return _then(_value.copyWith(
+      sessionName: null == sessionName
+          ? _value.sessionName
+          : sessionName // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      user: null == user
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as TalkUser,
+      contents: null == contents
+          ? _value.contents
+          : contents // ignore: cast_nullable_to_non_nullable
+              as String,
+      time: null == time
+          ? _value.time
+          : time // ignore: cast_nullable_to_non_nullable
+              as String,
+      trackName: null == trackName
+          ? _value.trackName
+          : trackName // ignore: cast_nullable_to_non_nullable
+              as String,
+      twitter: null == twitter
+          ? _value.twitter
+          : twitter // ignore: cast_nullable_to_non_nullable
+              as String,
+      isSponsor: null == isSponsor
+          ? _value.isSponsor
+          : isSponsor // ignore: cast_nullable_to_non_nullable
+              as bool,
+      tweet: null == tweet
+          ? _value.tweet
+          : tweet // ignore: cast_nullable_to_non_nullable
+              as String,
+      forteeUrl: null == forteeUrl
+          ? _value.forteeUrl
+          : forteeUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      sponsorImage: freezed == sponsorImage
+          ? _value.sponsorImage
+          : sponsorImage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      sponsorName: freezed == sponsorName
+          ? _value.sponsorName
+          : sponsorName // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $TalkUserCopyWith<$Res> get user {
+    return $TalkUserCopyWith<$Res>(_value.user, (value) {
+      return _then(_value.copyWith(user: value) as $Val);
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$_SessionModelCopyWith<$Res>
+    implements $SessionModelCopyWith<$Res> {
+  factory _$$_SessionModelCopyWith(
+          _$_SessionModel value, $Res Function(_$_SessionModel) then) =
+      __$$_SessionModelCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String sessionName,
+      String title,
+      TalkUser user,
+      String contents,
+      String time,
+      String trackName,
+      String twitter,
+      bool isSponsor,
+      String tweet,
+      String forteeUrl,
+      String? sponsorImage,
+      String? sponsorName});
+
+  @override
+  $TalkUserCopyWith<$Res> get user;
+}
+
+/// @nodoc
+class __$$_SessionModelCopyWithImpl<$Res>
+    extends _$SessionModelCopyWithImpl<$Res, _$_SessionModel>
+    implements _$$_SessionModelCopyWith<$Res> {
+  __$$_SessionModelCopyWithImpl(
+      _$_SessionModel _value, $Res Function(_$_SessionModel) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? sessionName = null,
+    Object? title = null,
+    Object? user = null,
+    Object? contents = null,
+    Object? time = null,
+    Object? trackName = null,
+    Object? twitter = null,
+    Object? isSponsor = null,
+    Object? tweet = null,
+    Object? forteeUrl = null,
+    Object? sponsorImage = freezed,
+    Object? sponsorName = freezed,
+  }) {
+    return _then(_$_SessionModel(
+      sessionName: null == sessionName
+          ? _value.sessionName
+          : sessionName // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      user: null == user
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as TalkUser,
+      contents: null == contents
+          ? _value.contents
+          : contents // ignore: cast_nullable_to_non_nullable
+              as String,
+      time: null == time
+          ? _value.time
+          : time // ignore: cast_nullable_to_non_nullable
+              as String,
+      trackName: null == trackName
+          ? _value.trackName
+          : trackName // ignore: cast_nullable_to_non_nullable
+              as String,
+      twitter: null == twitter
+          ? _value.twitter
+          : twitter // ignore: cast_nullable_to_non_nullable
+              as String,
+      isSponsor: null == isSponsor
+          ? _value.isSponsor
+          : isSponsor // ignore: cast_nullable_to_non_nullable
+              as bool,
+      tweet: null == tweet
+          ? _value.tweet
+          : tweet // ignore: cast_nullable_to_non_nullable
+              as String,
+      forteeUrl: null == forteeUrl
+          ? _value.forteeUrl
+          : forteeUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      sponsorImage: freezed == sponsorImage
+          ? _value.sponsorImage
+          : sponsorImage // ignore: cast_nullable_to_non_nullable
+              as String?,
+      sponsorName: freezed == sponsorName
+          ? _value.sponsorName
+          : sponsorName // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_SessionModel implements _SessionModel {
+  const _$_SessionModel(
+      {required this.sessionName,
+      required this.title,
+      required this.user,
+      required this.contents,
+      required this.time,
+      required this.trackName,
+      required this.twitter,
+      required this.isSponsor,
+      required this.tweet,
+      required this.forteeUrl,
+      this.sponsorImage,
+      this.sponsorName});
+
+  @override
+  final String sessionName;
+  @override
+  final String title;
+  @override
+  final TalkUser user;
+  @override
+  final String contents;
+  @override
+  final String time;
+  @override
+  final String trackName;
+  @override
+  final String twitter;
+  @override
+  final bool isSponsor;
+  @override
+  final String tweet;
+  @override
+  final String forteeUrl;
+  @override
+  final String? sponsorImage;
+  @override
+  final String? sponsorName;
+
+  @override
+  String toString() {
+    return 'SessionModel(sessionName: $sessionName, title: $title, user: $user, contents: $contents, time: $time, trackName: $trackName, twitter: $twitter, isSponsor: $isSponsor, tweet: $tweet, forteeUrl: $forteeUrl, sponsorImage: $sponsorImage, sponsorName: $sponsorName)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_SessionModel &&
+            (identical(other.sessionName, sessionName) ||
+                other.sessionName == sessionName) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.user, user) || other.user == user) &&
+            (identical(other.contents, contents) ||
+                other.contents == contents) &&
+            (identical(other.time, time) || other.time == time) &&
+            (identical(other.trackName, trackName) ||
+                other.trackName == trackName) &&
+            (identical(other.twitter, twitter) || other.twitter == twitter) &&
+            (identical(other.isSponsor, isSponsor) ||
+                other.isSponsor == isSponsor) &&
+            (identical(other.tweet, tweet) || other.tweet == tweet) &&
+            (identical(other.forteeUrl, forteeUrl) ||
+                other.forteeUrl == forteeUrl) &&
+            (identical(other.sponsorImage, sponsorImage) ||
+                other.sponsorImage == sponsorImage) &&
+            (identical(other.sponsorName, sponsorName) ||
+                other.sponsorName == sponsorName));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      sessionName,
+      title,
+      user,
+      contents,
+      time,
+      trackName,
+      twitter,
+      isSponsor,
+      tweet,
+      forteeUrl,
+      sponsorImage,
+      sponsorName);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_SessionModelCopyWith<_$_SessionModel> get copyWith =>
+      __$$_SessionModelCopyWithImpl<_$_SessionModel>(this, _$identity);
+}
+
+abstract class _SessionModel implements SessionModel {
+  const factory _SessionModel(
+      {required final String sessionName,
+      required final String title,
+      required final TalkUser user,
+      required final String contents,
+      required final String time,
+      required final String trackName,
+      required final String twitter,
+      required final bool isSponsor,
+      required final String tweet,
+      required final String forteeUrl,
+      final String? sponsorImage,
+      final String? sponsorName}) = _$_SessionModel;
+
+  @override
+  String get sessionName;
+  @override
+  String get title;
+  @override
+  TalkUser get user;
+  @override
+  String get contents;
+  @override
+  String get time;
+  @override
+  String get trackName;
+  @override
+  String get twitter;
+  @override
+  bool get isSponsor;
+  @override
+  String get tweet;
+  @override
+  String get forteeUrl;
+  @override
+  String? get sponsorImage;
+  @override
+  String? get sponsorName;
+  @override
+  @JsonKey(ignore: true)
+  _$$_SessionModelCopyWith<_$_SessionModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
