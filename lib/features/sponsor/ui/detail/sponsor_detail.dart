65a5c79db69ab7b2df95b1d4ac14cc4b37582bcc
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-09-25 22:00:27 +0900

    refactor: スポンサーデータを動的に取得

--- lib/features/sponsor/ui/detail/sponsor_detail.dart
+++ lib/features/sponsor/ui/detail/sponsor_detail.dart
@@ -1,9 +1,6 @@
 import 'package:confwebsite2023/core/components/responsive_widget.dart';
-import 'package:confwebsite2023/core/gen/assets.gen.dart';
 import 'package:confwebsite2023/core/theme.dart';
-import 'package:confwebsite2023/features/sponsor/data/sponsor.dart';
-import 'package:confwebsite2023/features/sponsor/data/sponsor_plan.dart';
-import 'package:confwebsite2023/features/sponsor/data/sponsor_session.dart';
+import 'package:confwebsite2023/features/sponsor/data/sponsor_provider.dart';
 import 'package:confwebsite2023/features/sponsor/ui/detail/sponsor_detail_content.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -13,8 +10,7 @@ final class SponsorDetail extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: データを動的に取得する
-    const sponsor = _sponsor;
+    final sponsor = ref.watch(sponsorProvider);
 
     return ResponsiveWidget(
       largeWidget: SponsorDetailContent(
@@ -42,29 +38,3 @@ final class SponsorDetail extends ConsumerWidget {
     );
   }
 }
-
-// TODO: データを動的に取得する
-const _sponsor = Sponsor(
-  name: 'flutterkaigi',
-  displayName: '企業名・ブランド名',
-  url: 'https://flutterkaigi.jp/2023/',
-  logoAssetName: Assets.flutterkaigiLogoShadowed,
-  plan: SponsorPlan.platinum,
-  session: SponsorSession(
-    id: 'id',
-    title: 'Flutterアプリ開発におけるモジュール分割戦略 〜dart15万行を100分割する〜',
-    url: 'https://flutterkaigi.jp/2023/',
-    scheduledAt: '2023年11月10日 11:10〜11:55（45分）',
-  ),
-  introduction: '紹介文（600字程度）\n紹介文（600字程度）\n紹介文（600字程度）',
-);
-
-// const _sponsor = Sponsor(
-//   name: 'flutterkaigi',
-//   displayName: '企業名・ブランド名',
-//   url: 'https://flutterkaigi.jp/2023/',
-//   logoAssetName: Assets.flutterkaigiLogoShadowed,
-//   plan: SponsorPlan.platinum,
-//   session: null,
-//   introduction: '紹介文（600字程度）\n紹介文（600字程度）\n紹介文（600字程度）',
-// );
