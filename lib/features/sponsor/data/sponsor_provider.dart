87f756abb6297d5d3879c4499d7badc04f3fec21
blendthink <blendthink@users.noreply.github.com>
2023-09-25 04:18:19 +0000

    Auto format

--- lib/features/sponsor/data/sponsor_provider.dart
+++ lib/features/sponsor/data/sponsor_provider.dart
@@ -1,6 +1,5 @@
 import 'package:collection/collection.dart';
 import 'package:confwebsite2023/features/sponsor/data/sponsor.dart';
-import 'package:confwebsite2023/features/sponsor/data/sponsor_data_source.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'sponsor_provider.g.dart';
