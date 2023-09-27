b9b611877f631f512f1e2ee7a0866ba88ee3d75c
Ryo Takeuchi <develop@r09i.me>
2023-09-27 09:29:20 +0900

    fix: シルバースポンサーの順番を変更

--- lib/features/sponsor/data/sponsor_data_source.dart
+++ lib/features/sponsor/data/sponsor_data_source.dart
@@ -131,26 +131,6 @@ List<Sponsor> _goldSponsors(_GoldSponsorsRef ref) => [
 @riverpod
 List<Sponsor> _silverSponsors(_SilverSponsorsRef ref) => [
       Sponsor(
-        name: 'cyberagent',
-        displayName: '株式会社サイバーエージェント',
-        url: 'https://www.cyberagent.co.jp/',
-        logoAssetName: Assets.sponsors.cyberagent.keyName,
-        plan: SponsorPlan.silver,
-        session: null,
-        introduction:
-            'サイバーエージェントは「21世紀を代表する会社を創る」をビジョンに掲げ、新しい未来のテレビ「ABEMA」の運営や国内トップシェアを誇るインターネット広告事業、ゲーム事業などITを主軸にしたビジネスを多角的に展開しています',
-      ),
-      Sponsor(
-        name: 'diverse',
-        displayName: '株式会社Diverse',
-        url: 'https://diverse-inc.co.jp/',
-        logoAssetName: Assets.sponsors.diverse,
-        plan: SponsorPlan.silver,
-        session: null,
-        introduction:
-            '累計1900万人が利用するマッチングサービス「YYC」を開発しています。YYCのiOSとAndroidはFlutterで開発しており、マッチング機能だけでなくライブ配信機能などもDartで実装しています。興味のある方はカジュアル面談まで！',
-      ),
-      Sponsor(
         name: 'enechain',
         displayName: '株式会社enechain',
         url: 'https://enechain.co.jp',
@@ -159,50 +139,6 @@ List<Sponsor> _silverSponsors(_SilverSponsorsRef ref) => [
         session: null,
         introduction:
             'enechainは国内最大のエネルギーマーケットプレイスを運営するスタートアップです。誰もが参加でき、あらゆるエネルギーの価値を交換できるフェアなマーケットを創ることで社会が秘めたる可能性を掘り起こし、人々の豊かさと世界の平和に貢献します。',
-      ),
-      Sponsor(
-        name: 'harmo',
-        displayName: 'harmo株式会社',
-        url: 'https://www.harmo.biz/',
-        logoAssetName: Assets.sponsors.harmo.keyName,
-        plan: SponsorPlan.silver,
-        session: null,
-        introduction:
-            '私たちは個々人のヘルスデータを活用した新サービスを生み出している会社です。 「harmoおくすり手帳」は、Flutter/Dartで現在、開発中。よりユーザーに使いやすい姿でリリース予定です。ご興味ありましたら是非ご連絡ください！',
-      ),
-      Sponsor(
-        name: 'layerx',
-        displayName: '株式会社LayerX',
-        url: 'https://layerx.co.jp/',
-        logoAssetName: Assets.sponsors.layerx,
-        plan: SponsorPlan.silver,
-        session: null,
-      ),
-      Sponsor(
-        name: 'm3',
-        displayName: 'エムスリー株式会社',
-        url: 'https://corporate.m3.com/',
-        logoAssetName: Assets.sponsors.mthree,
-        plan: SponsorPlan.silver,
-        session: null,
-      ),
-      Sponsor(
-        name: 'pioneer',
-        displayName: 'パイオニア株式会社',
-        url: 'https://jpn.pioneer/ja/',
-        logoAssetName: Assets.sponsors.pioneer,
-        plan: SponsorPlan.silver,
-        session: null,
-        introduction:
-            'パイオニアは「より多くの人と、感動を」という企業理念のもと、数々の世界初、業界初の商品を展開しています。現在、企業ビジョン“未来の移動体験を創ります”の実現に向け、モビリティ領域におけるソリューションサービス企業への変革に取り組んでいます。',
-      ),
-      Sponsor(
-        name: 'recruit',
-        displayName: '株式会社リクルート',
-        url: 'https://www.recruit.co.jp/',
-        logoAssetName: Assets.sponsors.recruit.keyName,
-        plan: SponsorPlan.silver,
-        session: null,
       ),
       Sponsor(
         name: 'studyplus',
@@ -215,6 +151,26 @@ List<Sponsor> _silverSponsors(_SilverSponsorsRef ref) => [
             'スタディプラス株式会社は「学ぶ喜びをすべての人へ」をミッションに掲げています Studyplusは学習内容を記録・可視化・共有することによりモチベーション維持、 学習の習慣化を補助します 私たちは学びに寄り添うプラットフォームを作ります',
       ),
       Sponsor(
+        name: 'pioneer',
+        displayName: 'パイオニア株式会社',
+        url: 'https://jpn.pioneer/ja/',
+        logoAssetName: Assets.sponsors.pioneer,
+        plan: SponsorPlan.silver,
+        session: null,
+        introduction:
+            'パイオニアは「より多くの人と、感動を」という企業理念のもと、数々の世界初、業界初の商品を展開しています。現在、企業ビジョン“未来の移動体験を創ります”の実現に向け、モビリティ領域におけるソリューションサービス企業への変革に取り組んでいます。',
+      ),
+      Sponsor(
+        name: 'diverse',
+        displayName: '株式会社Diverse',
+        url: 'https://diverse-inc.co.jp/',
+        logoAssetName: Assets.sponsors.diverse,
+        plan: SponsorPlan.silver,
+        session: null,
+        introduction:
+            '累計1900万人が利用するマッチングサービス「YYC」を開発しています。YYCのiOSとAndroidはFlutterで開発しており、マッチング機能だけでなくライブ配信機能などもDartで実装しています。興味のある方はカジュアル面談まで！',
+      ),
+      Sponsor(
         name: 'tokyu',
         displayName: '東急株式会社',
         url: 'https://10q89s.jp/',
@@ -223,5 +179,49 @@ List<Sponsor> _silverSponsors(_SilverSponsorsRef ref) => [
         session: null,
         introduction:
             '東急URBAN HACKSは「街づくりのDX」を実現するための内製開発チームです。東急グループの次の100年を変革する大プロジェクト。あなたの技術や知識を活かし、一緒に世界一の街づくりを目指してみませんか？',
+      ),
+      Sponsor(
+        name: 'm3',
+        displayName: 'エムスリー株式会社',
+        url: 'https://corporate.m3.com/',
+        logoAssetName: Assets.sponsors.mthree,
+        plan: SponsorPlan.silver,
+        session: null,
+      ),
+      Sponsor(
+        name: 'cyberagent',
+        displayName: '株式会社サイバーエージェント',
+        url: 'https://www.cyberagent.co.jp/',
+        logoAssetName: Assets.sponsors.cyberagent.keyName,
+        plan: SponsorPlan.silver,
+        session: null,
+        introduction:
+            'サイバーエージェントは「21世紀を代表する会社を創る」をビジョンに掲げ、新しい未来のテレビ「ABEMA」の運営や国内トップシェアを誇るインターネット広告事業、ゲーム事業などITを主軸にしたビジネスを多角的に展開しています',
+      ),
+      Sponsor(
+        name: 'harmo',
+        displayName: 'harmo株式会社',
+        url: 'https://www.harmo.biz/',
+        logoAssetName: Assets.sponsors.harmo.keyName,
+        plan: SponsorPlan.silver,
+        session: null,
+        introduction:
+            '私たちは個々人のヘルスデータを活用した新サービスを生み出している会社です。 「harmoおくすり手帳」は、Flutter/Dartで現在、開発中。よりユーザーに使いやすい姿でリリース予定です。ご興味ありましたら是非ご連絡ください！',
+      ),
+      Sponsor(
+        name: 'recruit',
+        displayName: '株式会社リクルート',
+        url: 'https://www.recruit.co.jp/',
+        logoAssetName: Assets.sponsors.recruit.keyName,
+        plan: SponsorPlan.silver,
+        session: null,
+      ),
+      Sponsor(
+        name: 'layerx',
+        displayName: '株式会社LayerX',
+        url: 'https://layerx.co.jp/',
+        logoAssetName: Assets.sponsors.layerx,
+        plan: SponsorPlan.silver,
+        session: null,
       ),
     ];
