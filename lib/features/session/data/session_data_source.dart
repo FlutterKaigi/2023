ed57d6fab0938479322855706e09d429f331562b
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-10-01 13:22:55 +0900

    fix: Exclude sessions whose track is not decided

--- lib/features/session/data/session_data_source.dart
+++ lib/features/session/data/session_data_source.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:confwebsite2023/features/session/data/session.dart';
 import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'session_data_source.g.dart';
@@ -23,8 +24,20 @@ class SessionDataSource {
     final itemsJson = jsonResult['timetable'] as List<dynamic>;
     return itemsJson
         .map(
-          (e) => Session.fromJson(e as Map<String, dynamic>),
+          (e) {
+            try {
+              return Session.fromJson(e as Map<String, dynamic>);
+            } on CheckedFromJsonException catch (e) {
+              if (e.key == 'track') {
+                // Exclude sessions whose track is not decided, such as
+                // substitute sessions.
+                return null;
+              }
+              rethrow;
+            }
+          },
         )
+        .nonNulls
         .toList();
   }
 }
