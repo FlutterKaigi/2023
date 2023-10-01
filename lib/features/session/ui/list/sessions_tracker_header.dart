f3c0bae8b9594068f36f93d2f5c53224c7d5655c
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-10-01 23:54:57 +0900

    feat: Use production data in the SessionsTrackHeader

--- lib/features/session/ui/list/sessions_tracker_header.dart
+++ lib/features/session/ui/list/sessions_tracker_header.dart
@@ -1,31 +1,31 @@
 import 'package:confwebsite2023/core/components/responsive_widget.dart';
 import 'package:confwebsite2023/core/theme/dimension.dart';
+import 'package:confwebsite2023/features/session/data/session_provider.dart';
 import 'package:confwebsite2023/features/session/data/track.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final class SessionsTrackHeader extends StatelessWidget {
+final class SessionsTrackHeader extends ConsumerWidget {
   const SessionsTrackHeader({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    const leftTrack = Track(name: 'Track１', sort: 1);
-    const rightTrack = Track(name: 'Track２', sort: 2);
-
-    return const ResponsiveWidget(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tracks = ref.watch(tracksProvider);
+    return ResponsiveWidget(
       largeWidget: Column(
         children: [
           Row(
             children: [
-              Expanded(child: _TrackCard(leftTrack)),
+              Expanded(child: _TrackCard(tracks.left)),
               Spaces.horizontal_20,
-              Expanded(child: _TrackCard(rightTrack)),
+              Expanded(child: _TrackCard(tracks.right)),
             ],
           ),
           Spaces.vertical_40,
         ],
       ),
-      smallWidget: SizedBox.shrink(),
+      smallWidget: const SizedBox.shrink(),
     );
   }
 }
