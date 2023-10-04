b3e98af69d971044a8b2cb6ba861bf387da96025
Ryo Takeuchi <develop@r09i.me>
2023-10-04 21:04:44 +0900

    feat: ホームページにアナウンスセクションを追加

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:confwebsite2023/core/components/responsive_widget.dart';
 import 'package:confwebsite2023/core/theme.dart';
 import 'package:confwebsite2023/features/access/ui/access_widget.dart';
+import 'package:confwebsite2023/features/announce/ui/announce_section.dart';
 import 'package:confwebsite2023/features/count_down/model/count_down_timer.dart';
 import 'package:confwebsite2023/features/count_down/ui/count_down_section.dart';
 import 'package:confwebsite2023/features/event/hands-on/ui/hands_on_event.dart';
@@ -32,6 +33,7 @@ class MainPage extends HookWidget {
       event: GlobalObjectKey('eventSectionKey'),
       ticket: GlobalObjectKey('ticketSectionKey'),
       session: GlobalObjectKey('sessionSectionKey'),
+      announce: GlobalObjectKey('announceSectionKey'),
       sponsor: GlobalObjectKey('sponsorSectionKey'),
       staff: GlobalObjectKey('staffSectionKey'),
     );
@@ -62,6 +64,10 @@ class MainPage extends HookWidget {
       HeaderItemButtonData(
         title: 'Event',
         onPressed: () async => scrollToSection(sectionKeys.event),
+      ),
+      HeaderItemButtonData(
+        title: 'Announce',
+        onPressed: () async => scrollToSection(sectionKeys.announce),
       ),
       HeaderItemButtonData(
         title: 'Sponsor',
@@ -107,6 +113,7 @@ class _MainPageBody extends StatelessWidget {
     GlobalObjectKey event,
     GlobalObjectKey ticket,
     GlobalObjectKey session,
+    GlobalObjectKey announce,
     GlobalObjectKey sponsor,
     GlobalObjectKey staff
   }) sectionKeys;
@@ -201,6 +208,15 @@ class _MainPageBody extends StatelessWidget {
               padding: padding,
               child: HandsOnEvent(
                 key: sectionKeys.event,
+              ),
+            ),
+            const SliverToBoxAdapter(
+              child: Spaces.vertical_200,
+            ),
+            _Sliver(
+              padding: padding,
+              child: AnnounceSection(
+                key: sectionKeys.announce,
               ),
             ),
             const SliverToBoxAdapter(
