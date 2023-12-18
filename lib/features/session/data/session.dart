470bf4971de4ad2989b6ea728f75442d806d1d90
iseruuuuu <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>
2023-12-18 12:39:13 +0900

    fix: switch文の修正をしました

--- lib/features/session/data/session.dart
+++ lib/features/session/data/session.dart
@@ -72,21 +72,19 @@ extension SessionTimeText on Session {
 }
 
 extension SessionYoutubeUrlEx on TalkSession {
-  String get youtubeUrl {
-    return switch (uuid) {
-      '21abbd32-6864-487a-8066-c9d7f7e5e9be' => 'cMS2FzbhXJ8',
-      '5b402df3-9e5d-4c0b-80fa-61d9ba356594' => 'bKnvn-Orpd0',
-      '972ffbac-422b-4d4b-9686-f59c4438da04' => 'jNmyr-TZVfU',
-      '84b1350e-b76e-4cdc-884b-37a4a6e14846' => 'UGxzjYNHH90',
-      '090ad5b8-7066-40e6-8ca8-58fff766f046' => 'lObSpRxP1Do',
-      '0b32515e-2c80-45f4-8ea2-c6c269d2609f' => '2SnTNFAzmY0',
-      'df52c995-5fbb-4ff0-abbc-e6332af98797' => 'VHhZlTDfwIQ',
-      '67df96a0-4f03-401a-b740-c296a5bcbd86' => 'NcfrY-EN8Pg',
-      'f76c37b8-172d-4072-ad4a-bd870bc15728' => 'sznsAolD6dI',
-      'd9cc75af-a3a2-4d0e-af6c-f12aa143ba4c' => 'EKoI-p1UnNk',
-      '076d093c-a1ff-4fe3-be58-8f8536c97de3' => 'vCoG6BTNpAA',
-      '2b0118b0-52d2-4a9c-a564-faf50651dea2' => '6zLih07J3RU',
-      _ => '',
-    };
-  }
+  String get youtubeUrl => switch (uuid) {
+    '21abbd32-6864-487a-8066-c9d7f7e5e9be' => 'cMS2FzbhXJ8',
+    '5b402df3-9e5d-4c0b-80fa-61d9ba356594' => 'bKnvn-Orpd0',
+    '972ffbac-422b-4d4b-9686-f59c4438da04' => 'jNmyr-TZVfU',
+    '84b1350e-b76e-4cdc-884b-37a4a6e14846' => 'UGxzjYNHH90',
+    '090ad5b8-7066-40e6-8ca8-58fff766f046' => 'lObSpRxP1Do',
+    '0b32515e-2c80-45f4-8ea2-c6c269d2609f' => '2SnTNFAzmY0',
+    'df52c995-5fbb-4ff0-abbc-e6332af98797' => 'VHhZlTDfwIQ',
+    '67df96a0-4f03-401a-b740-c296a5bcbd86' => 'NcfrY-EN8Pg',
+    'f76c37b8-172d-4072-ad4a-bd870bc15728' => 'sznsAolD6dI',
+    'd9cc75af-a3a2-4d0e-af6c-f12aa143ba4c' => 'EKoI-p1UnNk',
+    '076d093c-a1ff-4fe3-be58-8f8536c97de3' => 'vCoG6BTNpAA',
+    '2b0118b0-52d2-4a9c-a564-faf50651dea2' => '6zLih07J3RU',
+    _ => '',
+  };
 }
